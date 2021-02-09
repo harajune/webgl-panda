66f80cf755621b7564fe42c4ba55ba22c5b99d8d
harajune <harajune@mercari.com>
2021-02-09 20:27:42 +0900

    correct output path of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        {from:'src/index.html', to:'build'} 
+        {from:'src/index.html', to:'./'} 
       ]
     }),
   ]
